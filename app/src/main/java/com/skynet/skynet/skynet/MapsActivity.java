8b0a9c396c7b2f8603149f9cc7af7acfa9c6e3fe
Saif Khan <sj5khan@uwaterloo.ca>
2016-04-22 23:33:56 -0400

    FragmentActivity -> AppCompactActivity

--- app/src/main/java/com/skynet/skynet/skynet/MapsActivity.java
+++ app/src/main/java/com/skynet/skynet/skynet/MapsActivity.java
@@ -1,14 +1,14 @@
 package com.skynet.skynet.skynet;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends AppCompatActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -40,6 +40,7 @@ public class MapsActivity extends FragmentActivity {
      * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
      * method in {@link #onResume()} to guarantee that it will be called.
      */
+
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
