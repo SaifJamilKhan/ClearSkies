094d6706ae26135785f259d46332d647db3b2d0e
Saif Khan <sj5khan@uwaterloo.ca>
2016-04-23 02:43:10 -0400

    Tool bar and my location work

--- app/src/main/java/com/skynet/skynet/skynet/MapsActivity.java
+++ app/src/main/java/com/skynet/skynet/skynet/MapsActivity.java
@@ -1,10 +1,17 @@
 package com.skynet.skynet.skynet;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -14,16 +21,28 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity implements LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Bind(R.id.my_toolbar)
+    public Toolbar myToolbar;
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
         ButterKnife.bind(this);
+        setUpMapIfNeeded();
+        mMap.setMyLocationEnabled(true);
+        myToolbar.setTitle("Hermes");
+        setSupportActionBar(myToolbar);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.PASSIVE_PROVIDER
     }
 
     @Override
@@ -35,8 +54,7 @@ public class MapsActivity extends AppCompatActivity {
     @OnClick(R.id.btn_map_toggle)
     public void mapTypeToggle() {
         if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
-        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-//        Toast.makeText(this, "Satellite", Toast.LENGTH_SHORT).show();
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         } else {
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
@@ -80,4 +98,35 @@ public class MapsActivity extends AppCompatActivity {
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mMap.animateCamera(cameraUpdate);
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+//
+//    @Override
+//    public void onLocationChanged(Location location) {
+//        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+//        mMap.animateCamera(cameraUpdate);
+//        locationManager.removeUpdates(this);
+//    }
 }
