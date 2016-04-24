6fe9fbc5b88c7e1c631db803cfa3abc246b24ef3
Saif Khan <sj5khan@uwaterloo.ca>
2016-04-24 00:22:52 -0400

    Reduce drone radius sizes.

--- app/src/main/java/com/skynet/skynet/skynet/Drone.java
+++ app/src/main/java/com/skynet/skynet/skynet/Drone.java
@@ -20,13 +20,13 @@ public class Drone {
             this.sizeLevel = jsonObject.getInt("size");
             switch (this.sizeLevel) {
                 case 0:
-                    this.radius = 5630; // 3.5 miles
+                    this.radius = 3000; // 3.5 miles
                     break;
                 case 1:
-                    this.radius = 8046; // 6.5 miles
+                    this.radius = 5000; // 6.5 miles
                     break;
                 case 2:
-                    this.radius = 11000; // 8.5 miles
+                    this.radius = 7000; // 8.5 miles
                     break;
             }
         } catch (JSONException e) {
